e93a41c23aae00781564bd2b10bdb893cea8c3d5
maheshkolluru <maheshkolluru4@gmail.com>
2024-02-16 11:43:22 +0530

    TESTNG ADDED

--- src/test/java/com/jenkinss/TC01Test.java
+++ src/test/java/com/jenkinss/TC01Test.java
@@ -1,12 +1,13 @@
 package com.jenkinss;
 
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class TC01Test {
 	@Test
 	public void m1()
 	{
-		
+	Reporter.log("m1 updated",true);	
 		
 	}
 
