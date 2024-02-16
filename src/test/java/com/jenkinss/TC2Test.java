8dfa0687a4eb79e3586d920a95613063226674dd
maheshkolluru <maheshkolluru4@gmail.com>
2024-02-16 11:54:44 +0530

    m2 updated

---
+++ src/test/java/com/jenkinss/TC2Test.java
@@ -0,0 +1,13 @@
+package com.jenkinss;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC2Test {
+	@Test
+	public void m2()
+	{
+		Reporter.log("m2 updated ",true);
+	}
+
+}
